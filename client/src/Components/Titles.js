a4f32ad850fe9a99a9491512a00128830ddfa178
Taylor Farley <twfarley88@gmail.com>
2021-03-25 15:30:24 -0400

    show nulls

--- client/src/Components/Titles.js
+++ client/src/Components/Titles.js
@@ -1,7 +1,10 @@
 import React, { useContext, useState, useEffect } from "react";
 import UserContext from "../Context/UserContext";
 import ReactDOM from "react-dom";
-import axios from 'axios'
+import axios from "axios";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import {
   DataSheetGrid,
   checkboxColumn,
@@ -14,7 +17,7 @@ const Titles = () => {
 
   const [data, setData] = useState();
 
-  const columns = [  
+  const columns = [
     textColumn({ title: "Title", key: "title" }),
     textColumn({ title: "Log", key: "log" }),
     textColumn({ title: "Org", key: "org" }),
@@ -22,24 +25,51 @@ const Titles = () => {
 
   useEffect(() => {
     axios.get("/data/dataGet").then((res) => {
-    setData(res.data)
-    console.log(res.data)
+      setData(res.data);
+      console.log(res.data);
     });
   }, []);
 
   useEffect(() => {
     axios.post("/data/dataSend", data).then((res) => {
-   console.log(res)
+      console.log(res);
     });
   }, [data]);
 
-  
+  const ShowBlanks = (event) => {
+    event.preventDefault();
+    let newArr = [];
+    data.map((currentElement, index) => {
+      if (currentElement.log == null) {
+        newArr.push(currentElement);
+      }
+    });
+    setData(newArr);
+  };
+
+  const ShowAll = (event) => {
+    event.preventDefault();
+    axios.get("/data/dataGet").then((res) => {
+      setData(res.data);    
+    });
+  };
 
 
   return (
     <div>
-   
-      {realUser.email ? <h1>{realUser.firstName.charAt(0).toUpperCase()+ realUser.firstName.slice(1)}'s Titles</h1> : null}
+      {realUser.email ? (
+        <h1>
+          {realUser.firstName.charAt(0).toUpperCase() +
+            realUser.firstName.slice(1)}
+          's Titles
+        </h1>
+      ) : null}
+      <Button variant="contained" color="primary" onClick={ShowBlanks}>
+        Show Nulls
+      </Button>
+      <Button variant="contained" color="primary" onClick={ShowAll}>
+        Show All
+      </Button>
       <DataSheetGrid data={data} onChange={setData} columns={columns} />
     </div>
   );
