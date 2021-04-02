b09f7e7d79d74312ea2ea30cbf5f2f7d5b80a55c
Taylor Farley <twfarley88@gmail.com>
2021-04-02 10:44:46 -0400

    good

--- client/src/Components/Import.js
+++ client/src/Components/Import.js
@@ -1,26 +1,145 @@
 import React, { useCallback, useState } from "react";
 import CSVReader from "react-csv-reader";
 import Preview from './Preview'
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Zoom from '@material-ui/core/Zoom';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import UpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { green } from '@material-ui/core/colors';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`action-tabpanel-${index}`}
+      aria-labelledby={`action-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `action-tab-${index}`,
+    'aria-controls': `action-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: "100%",
+    position: 'relative',
+    minHeight: 200,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  fabGreen: {
+    color: theme.palette.common.white,
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[600],
+    },
+  },
+}));
 
 export default function Import() {
-    let [mydata, setmydata] = useState()
-   
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+  let [mydata, setmydata] = useState()
   const handleForce = (data, fileInfo) => {
-      console.log(data, fileInfo)
-       setmydata(data)
+    console.log(data, fileInfo)
+     setmydata(data)
+};
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
 
-
+  const transitionDuration = {
+    enter: theme.transitions.duration.enteringScreen,
+    exit: theme.transitions.duration.leavingScreen,
+  };
   const papaparseOptions = {
     header: true,
     dynamicTyping: true,
     skipEmptyLines: true,
     transformHeader: (header) => header.toLowerCase().replace(/\W/g, "_"),
   };
+  const fabs = [
+    {
+      color: 'primary',
+      className: classes.fab,
+      icon: <AddIcon />,
+      label: 'Add',
+    },
+    {
+      color: 'secondary',
+      className: classes.fab,
+      icon: <EditIcon />,
+      label: 'Edit',
+    },
+    {
+      color: 'inherit',
+      className: clsx(classes.fab, classes.fabGreen),
+      icon: <UpIcon />,
+      label: 'Expand',
+    },
+  ];
 
   return (
-    <React.Fragment>
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="action tabs example"
+        >
+          <Tab label="CSV" {...a11yProps(0)} />
+          <Tab label="JSON" {...a11yProps(1)} />
+          <Tab label="XLSX" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+        <React.Fragment>
     <CSVReader
       cssClass="react-csv-input"
       onFileLoaded={handleForce}
@@ -29,5 +148,16 @@ export default function Import() {
     {mydata?(<Preview mydata={mydata} />):null}
   
     </React.Fragment>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+ 
+    </div>
   );
 }
+
